1cb55017538b3475563698aaced2c533e0eace8a
ajiteshk <ajiteshkahlon51@gmail.com>
2022-05-06 14:02:48 +0530

    Added axios call

---
+++ src/Components/ClippingsData/ClippingsData.js
@@ -0,0 +1,7 @@
+import { useState } from "react";
+
+const ClippingsData = () => {
+  const [authors, setAuthors] = useState([]);
+  const [titles, setTitles] = useState([]);
+  const []
+};
---
+++ src/Components/fileUpload.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+
+const UploadFile = () => {
+  const sendClipping = (file) => {
+    console.log("FILE", file);
+
+    let formData = new FormData();
+    formData.append("clipping", file);
+
+    const payload = {
+      method: "POST",
+      url: "http://localhost:8080/",
+      data: formData,
+    };
+
+    axios.post("http://localhost:8080/", payload).then((res) => {
+      console.log("RESPONSE", res.data);
+    });
+  };
+  return (
+    <>
+      <label htmlFor="clipping-upload"> Upload the MyClippings.txt file</label>
+      <input type="file" name="clipping" id="clipping-upload" />
+      <input
+        type="submit"
+        onClick={(e) => {
+          sendClipping(e.target.files[0]);
+        }}
+      />
+    </>
+  );
+};
+
+export default UploadFile;
